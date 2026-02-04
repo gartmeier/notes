db5b898bea822854cc45ba5b44b75f1227f6a2b4
Joshua Gartmeier <joshua@gartmeier.dev>
2026-02-04 11:59:22 +0100

    Setup Spring Boot project with Gradle #1

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Justitia Showcase
+# Notes
 
 
 
---
+++ src/main/java/dev/gartmeier/notes/controllers/HelloController.java
@@ -0,0 +1,14 @@
+package dev.gartmeier.notes.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String index() {
+        return "Hello, World2!";
+    }
+
+}
