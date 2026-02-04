8b7b72c2fbea33980f071820b080d3300a03945e
Joshua Gartmeier <joshua@gartmeier.dev>
2026-02-04 20:22:50 +0100

    Add Keycloak JWT authentication

--- src/test/java/dev/gartmeier/notes/ApplicationTests.java
+++ src/test/java/dev/gartmeier/notes/ApplicationTests.java
@@ -2,9 +2,14 @@ package dev.gartmeier.notes;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
 
 @SpringBootTest
 class ApplicationTests {
+
+	@MockitoBean
+	JwtDecoder jwtDecoder;
 
 	@Test
 	void contextLoads() {
